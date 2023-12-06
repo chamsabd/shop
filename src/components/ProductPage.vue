3a677ce583e9d76747b6772537ef96fd012d64aa
chamsabd <chamsabdelwahed42@gmail.com>
2023-12-07 00:55:19 +0100

    create products page+component

---
+++ src/components/ProductPage.vue
@@ -0,0 +1,62 @@
+<template>
+  
+    <b-card 
+    :title="product.title"
+    :img-src="product.image"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2 "
+  >
+    <b-card-text>
+     {{ product.desc }}
+    </b-card-text>
+
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+    
+</template>
+
+<script>
+export default {
+  name: 'ProductPage',
+   data: function () {
+        return {
+           
+        }
+    },
+  props: {
+    
+    product: {
+      type: Object,
+      required: true,
+    },
+    
+  },
+  created: function () {
+    console.log('user data from parent component:')
+   
+   
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+// h3 {
+//   margin: 40px 0 0;
+// }
+// ul {
+//   list-style-type: none;
+//   padding: 0;
+// }
+// li {
+//   display: inline-block;
+//   margin: 0 10px;
+// }
+// a {
+//   color: #42b983;
+// }
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,126 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <h1>welcome to our azza magazine</h1>
+  
+   <div v-if="products.length > 0" class="animated fadeIn ">
+ <ul type="none"><li center><h2> most popular products</h2>
+  <b-card-group deck >
+    <ProductPage  class="line"   v-for="(product,index) in currentPageProducts" :key="index" :product="product" />
+  </b-card-group>
+
+
+  <div class="card-pagination">
+        <div class="page-index" v-for="i in nbPages" :key="i"  @click="goto(i)" :class={active:currentPage(i)}></div>
+    </div>
+</li>
+  <li> <h2>all our products</h2>
+    <hr style="align-content: center;" color="blue" size="6px" width="50%">
+    <b-card-group deck >
+    <ProductPage  class="line"   v-for="(product,index) in products" :key="index" :product="product" />
+  </b-card-group>
+</li>
+</ul>
+</div>
+
+  <div v-else >Sorry there are no product in azza magazine for the moment</div>
+  
+
+   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ProductPage from '@/components/ProductPage.vue'
 
 export default {
   name: 'HomeView',
+  data: function () {
+        return {
+            products: [{image:require('@/assets/logo.png'),name:"p1",prix:"12DT",desc:" Lorem ipsum dolor sit amet.lorem9 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique."},
+            {image:require('@/assets/skin_care.jpg'),name:"olay",prix:"12DT",desc:" Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, fugit? Repudiandae dicta modi ea fugit maxime doloremque quae cum id? Lorem ipsum dolor sit amet.lorem9 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique."},
+            {image:require('@/assets/logo.png'),name:"p1",prix:"12DT",desc:" Lorem ipsum dolor sit amet.lorem9 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique."},
+            {image:require('@/assets/skin_care.jpg'),name:"olay",prix:"12DT",desc:" Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, fugit? Repudiandae dicta modi ea fugit maxime doloremque quae cum id? Lorem ipsum dolor sit amet.lorem9 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique."},
+            {image:require('@/assets/logo.png'),name:"p1",prix:"12DT",desc:" Lorem ipsum dolor sit amet.lorem9 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique."}],
+       
+            popularproducts: [{image:require('@/assets/logo.png'),name:"p1",prix:"12DT",desc:" Lorem ipsum dolor sit amet.lorem9 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique."},
+            {image:require('@/assets/skin_care.jpg'),name:"olay",prix:"12DT",desc:" Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, fugit? Repudiandae dicta modi ea fugit maxime doloremque quae cum id? Lorem ipsum dolor sit amet.lorem9 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique."},
+            {image:require('@/assets/logo.png'),name:"p1",prix:"12DT",desc:" Lorem ipsum dolor sit amet.lorem9 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique."},
+            {image:require('@/assets/logo.png'),name:"p1",prix:"12DT",desc:" Lorem ipsum dolor sit amet.lorem9 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique."},
+            {image:require('@/assets/logo.png'),name:"p1",prix:"12DT",desc:" Lorem ipsum dolor sit amet.lorem9 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique."},
+       
+           ],
+           paginatedProducts:[],
+      nbPages:0,
+      nbRowPerPage:3,
+      currentPageIndex:0
+       
+          }
+    },
+    computed: {
+      formattedProducts() {
+          return this.popularproducts.reduce((c, n, i) => {
+              if (i % 4 === 0) c.push([]);
+              c[c.length - 1].push(n);
+              return c;
+          }, []);
+      },
+      currentPageProducts(){
+         this.createPages();
+
+        return this.paginatedProducts[this.currentPageIndex];
+      }
+  },
   components: {
-    HelloWorld
+    ProductPage,
+  
+  },methods:{
+    currentPage(i){
+        return i-1===this.currentPageIndex;
+    },
+      createPages() {
+
+      let lengthAll = Object.keys(this.popularproducts).length;
+      this.nbPages = 0;
+       for (let i = 0; i < lengthAll; i = i + this.nbRowPerPage) {
+        this.paginatedProducts[this.nbPages] = this.popularproducts.slice(
+          i,
+          i + this.nbRowPerPage
+        );
+        this.nbPages++;
+      }
+    },
+    goto(i){
+
+      this.currentPageIndex=i-1;
+    }
   }
-}
+  }
+
 </script>
+
+<style scoped lang="scss">
+.card-pagination{
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  padding:20px;
+}
+.page-index{
+  margin-left:10px;
+  width:15px;
+  height:15px;
+  border-radius:15px;
+  background:#007bff
+}
+.active{
+   width:20px;
+  height:20px;
+  border-radius:20px;
+}
+.line {
+  display: inline-block; /* the default for span */
+  margin: 5px;
+  padding: auto;
+
+}
+</style>
